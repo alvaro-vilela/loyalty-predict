9e9939f539d6cba20906c6f9bf0b3498db97405e
Alvaro Moura <alvarovilela.m@gmail.com>
2025-12-17 10:32:06 -0300

    frequency update

---
+++ src/analytics/frequencia_valor.sql
@@ -0,0 +1,34 @@
+with tb_freq_valor AS (
+    SELECT 
+        idCliente,
+        count(DISTINCT substr(DtCriacao, 0, 11)) AS qtdeFrequencia,
+        sum(CASE WHEN QtdePontos > 0 THEN QtdePontos ELSE 0 END) AS qtdePontosPos
+
+    FROM transacoes
+
+    WHERE DtCriacao < '2025-09-01'
+    AND DtCriacao >= date('2025-09-01', '-28 day')
+
+    GROUP BY idCliente
+
+    ORDER BY qtdeFrequencia DESC
+),
+
+tb_cluster AS (
+    SELECT
+        *,
+        CASE
+            WHEN qtdeFrequencia <= 10 AND qtdePontosPos >= 1500 THEN '12-HYPER'
+            WHEN qtdeFrequencia > 10 AND qtdePontosPos > 1500 THEN '22-EFICIENTE'
+            WHEN qtdeFrequencia <= 10 AND qtdePontosPos >= 750 THEN '11-INDECISO'
+            WHEN qtdeFrequencia > 10 AND qtdePontosPos >= 750 THEN '21-ESFORÇADO'
+            WHEN qtdeFrequencia < 5 THEN '0-LURKER'
+            WHEN qtdeFrequencia <= 10 THEN '01-PREGUIÇOSO'
+            WHEN qtdeFrequencia > 10 THEN '20-POTENCIAL'
+        END AS cluster
+
+    FROM tb_freq_valor
+)
+
+SELECT *
+FROM tb_cluster
